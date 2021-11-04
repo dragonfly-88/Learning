93916380db64371ce431ca461e5758f4c7b100ca
Anilla Bodo <anilla.bodo@gmail.com>
2021-11-04 16:04:41 +0100

    Uploading folder

---
+++ 2. Orientation/HTML-basics/dealing-with-files.ts
@@ -0,0 +1,26 @@
+/*
+
+Choose a place to store your website projects.
+Inside your chosen place, create a new folder called web-projects (or similar).
+This is where all your website projects will live.
+
+
+test-site folder:
+
+index.html
+images folder
+styles folder
+scripts folder
+
+
+
+// reference where your file can be found
+
+<img src="images/your-image-filename" alt="My test image">
+
+vagy:
+
+<img src="../subdirectory/another-subdirectory/my-image.jpg" alt="My test image">
+
+
+*/
