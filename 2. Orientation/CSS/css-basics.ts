4af61e856f4c968b9f210d7da784a994b37f1d07
Anilla Bodo <anilla.bodo@gmail.com>
2021-11-04 19:49:32 +0100

    Saving updates

---
+++ 2. Orientation/CSS/css-basics.ts
@@ -0,0 +1,48 @@
+/*
+
+// What is CSS?
+
+- it controls the presentation of HTML
+- currently CSS3, but it's the same thing (evolving standards)
+- styling language
+
+
+// Default Browser Styles
+
+- all browers apply basic styling to HTML:
+- margin, padding, font size, underlining links etc
+- browers apply their own default styles
+
+
+// Basic CSS Syntax
+
+CSS is a collection of rules - each rule target a specific tag or element on an HTML doc
+#id  - targeting all tags
+#nav  - targeting all 'a' tags
+
+2 components:
+- selector      - declaration
+#page-header { font-size: 10px;
+                color: red;
+            }
+
+#header p { declaration }   // p tags within the header
+.comment div { declaration }  // comment classes - div elements
+li { declaration }  // li tags
+
+
+#header {
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+
+
+
+// Your First Web Page
+
+
+
+
+
+*/
---
+++ 2. Orientation/CSS/css-layout-basics.ts
@@ -0,0 +1,48 @@
+/*
+
+// text decoration
+
+article{
+    text-decoration: none;  // vagy: overline, inherit, underline, line-through
+}
+
+
+// Font Weight
+
+h1{
+    font-weight: bold;   // or normal, lighter, etc  400-600 normal range
+}
+
+
+// Text Transform
+
+#header p{
+    text-transform: capitalize    // or: none, full-width, inherit, lowercase, none, uppercase
+}
+
+
+// Styling Links
+
+<a href="#">This is a link</a>
+then: a{
+    color: crimson;
+    text-decoration: none;   // not underlined
+    font-weight: bold;
+}
+
+// hovering style
+
+a:hover{
+    color:darkmagenta;
+    text-decoration: underline;
+    background-color: aquamarine;
+}
+
+
+// Letter Spacing & Line Height
+
+
+
+
+
+*/
