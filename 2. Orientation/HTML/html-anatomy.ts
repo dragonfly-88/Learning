ddd95c2e80dde853110835b900e70bc19d6c2346
Anilla Bodo <anilla.bodo@gmail.com>
2021-11-04 20:03:57 +0100

    Saving last updates

---
+++ 2. Orientation/HTML/html-anatomy.ts
@@ -0,0 +1,79 @@
+/*
+
+<!DOCTYPE html >
+<html>
+<head>
+<meta charset="utf-8" >
+<title>My test page < /title>
+</head>
+<body >
+<img src="images/firefox-icon.png" alt = "My test image" >
+</body>
+</html>
+
+
+
+// images
+< img src = "images/firefox-icon.png" alt = "My test image">
+< img src = "images/firefox-icon.png" width="100%">  // browser szélessége lesz
+
+
+// headings
+
+<h1>My main title < /h1>
+< h2 > My top level heading < /h2>
+< h3 > My subheading < /h3>
+< h4 > My sub - subheading < /h4>
+
+
+// unordered & ordered lists
+
+< p > At Mozilla, we’re a global community of < /p>
+
+< ul >
+<litechnologists < /li>
+< li > thinkers < /li>
+< li > builders < /li>
+< /ul>
+
+< p > working together ... </p>
+
+
+
+// links (<a> stands for anchor)
+
+<a>Mozilla Manifesto</a>
+<a href="">Mozilla Manifesto</a>
+<a href="https://www.mozilla.org/en-US/about/manifesto/">Mozilla Manifesto</a>
+
+<a href="contact.html">Contact Us</a>
+
+
+// meta tag, css reference
+
+<head>
+    <meta name="keywords" content="SEO, words, for, Google">
+    <meta name="description" content="Whatever descr we want Google to bring up about the page">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+
+
+
+// headings
+
+<h1> </h1>  // most important heading
+<h2> </h2>   // max 4-5-öt használunk
+
+
+
+// different tags
+
+<strong>  // bold text
+<small>   // slightly smaller text
+<mark>    // ~ highlighting
+<sub>     // text below the line
+<sup>     // text above the line
+<pre>     // pre-format the text
+
+
+*/
